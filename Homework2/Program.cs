45b56183556015e03ab3b6021f3443159fc4b8d9
Kuvaev-dev <likeaboss13371@gmail.com>
2025-01-09 03:19:00 +0200

    Homework 2 Added

---
+++ Homework2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание: Проверить число на чётность с использованием if-else
+
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.OutputEncoding = Encoding.Unicode;
+
+                Console.WriteLine("Введите ваше число: ");
+                double.TryParse(Console.ReadLine(), out double value);
+
+                if (value % 2 == 0) Console.WriteLine("Число чётное");
+                else Console.WriteLine("Число нечётное");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
